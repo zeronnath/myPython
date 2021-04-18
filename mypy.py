5e06e5ef5a1bd028eb15a58dd48d40a31a7dc52a
zeronnath <59784598+zeronnath@users.noreply.github.com>
2021-04-18 15:41:56 +0900

    .

--- mypy.py
+++ mypy.py
@@ -1,5 +1,63 @@
+print(' ㅡㅡㅡ practice hard !! ㅡㅡ build your skills !! ㅡㅡㅡ')
 
-print('21332')
+### 책: 이것이 코딩 테스트다
+
+#  구현: 115p  Royal Knight
+
+# in each location on the chessboard, validate whether
+# a move is within the boundary
+# for the 8 moves of the knight.
+# 2->1 or 1->2
+# 2 kinds of moves x 4 directions. = 8 kinds of moves
+
+# interpret numeric coordinate into chessboard coordinate x->a~h, y->1~8
+# x, y = map(int, input().split())
+# nx = ny = 0
+# count =0
+#
+# moves = [[2, 1], [2, -1], [-2, 1], [-2, -1],
+#          [1, 2], [1, -2], [-1, 2], [-1, -2]]
+#
+# for i in range(len(moves)):
+#     nx = x+ moves[i][0]
+#     ny = y+moves[i][1]
+#     if 1 <=  nx  <= 8 and 1 <= ny <= 8:
+#         count +=1
+# print('count=',count)
+
+
+
+
+# ***** :  시분초 모든경우수 24x60x60 < 10만 연산 이하, 2초 내에.
+
+###  count '3' in time
+# h = int( input())
+# c = 0
+#
+# for m in range(h+1):
+#     for j in range(60):
+#         for i in range(60):
+#             if '3' in str(m)+str(j)+str(i):
+#                 c+=1
+
+#
+# for m in range(n+1):
+#     for l in range(6):
+#         for k in range(10):
+#             for j in range(6):
+#                 for i in range(10):
+#                     if i == 3:
+#                         c += 1
+#                 if j == 3:
+#                     c += 1
+#             if k == 3:
+#                 c += 1
+#         if l == 3:
+#             c += 1
+#     if n == 3:
+#         c += 1
+# print(c)
+
 ### traveller's [x, y]
 # n = int(input())
 # course = input().split()
@@ -21,7 +79,6 @@ print('21332')
 #             y+=1
 #     print(x, y)
 # print('goal = ' ,x,y)
-
 
 
 # # until n = 1
@@ -46,3 +103,21 @@ print('21332')
 #     count += target - 1
 #
 # #print('f count=',count)
+
+
+### minmal number of coins to compose an amount
+#
+# n = input()
+# count=0
+# remainder=int(n)
+# coins = [500, 100, 50, 10]
+# while(remainder>1):
+#     for coin in coins:
+#         q = remainder // coin
+#         if q > 0:
+#             count += q
+#             remainder %= coin
+#             break
+#
+#     print('remainder=', remainder)
+# print('count=', count)
